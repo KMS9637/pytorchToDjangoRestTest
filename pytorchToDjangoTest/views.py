0b884f8197586cd6abccdeaf3201c153330480b9
이상용 <lsy3709@naver.com>
2024-08-21 12:51:06 +0900

    샘플 100장으로 망치, 가위 확인

--- pytorchToDjangoTest/views.py
+++ pytorchToDjangoTest/views.py
@@ -9,95 +9,10 @@ import io
 from torch import nn
 import numpy as np
 import torch.nn.functional as F
-# import libmr
-
-#OpenMax, 이용
-# #1
-# class WeibullFitting:
-#     def __init__(self, tailsize=20):
-#         self.tailsize = tailsize
-#         self.weibull_models = {}
-#
-#     def fit(self, activations, labels):
-#         unique_labels = np.unique(labels)
-#         for label in unique_labels:
-#             class_activations = activations[labels == label]
-#             mean_vec = np.mean(class_activations, axis=0)
-#             distances = np.linalg.norm(class_activations - mean_vec, axis=1)
-#             mr = libmr.MR()
-#             mr.fit_high(distances, len(distances))
-#             self.weibull_models[label] = (mean_vec, mr)
-#
-#     def wscore(self, activations):
-#         scores = []
-#         for label, (mean_vec, mr) in self.weibull_models.items():
-#             distances = np.linalg.norm(activations - mean_vec, axis=1)
-#             wscores = mr.w_score(distances)
-#             scores.append(wscores)
-#         return np.array(scores)
-#
-# #2
-# def compute_openmax(activation_vector, weibull_model, alpha=10):
-#     ranked_activations = np.sort(activation_vector)[::-1]
-#     top_indices = np.argsort(activation_vector)[::-1][:alpha]
-#
-#     wscore = weibull_model.wscore(ranked_activations[top_indices])
-#
-#     openmax_vector = np.copy(activation_vector)
-#     openmax_vector[top_indices] -= openmax_vector[top_indices] * wscore
-#
-#     openmax_unknown_score = np.sum(activation_vector[top_indices] * wscore)
-#     openmax_vector = np.append(openmax_vector, openmax_unknown_score)
-#
-#     openmax_probs = F.softmax(torch.tensor(openmax_vector), dim=0).numpy()
-#     return openmax_probs
-#
-# #3
-# # Pretrained ResNet50 model
-# model = models.resnet50(pretrained=True)
-# model.eval()
-#
-#
-# # 이미지에서 특징 벡터 추출
-# def extract_features(image, model):
-#     with torch.no_grad():
-#         features = model(image)
-#     return features.numpy()
-#
-#
-# # Example of fitting Weibull model using activation vectors
-# def fit_weibull_for_model(train_loader, model, weibull_model):
-#     activations = []
-#     labels = []
-#
-#     for images, targets in train_loader:
-#         features = extract_features(images, model)
-#         activations.append(features)
-#         labels.append(targets.numpy())
-#
-#     activations = np.concatenate(activations, axis=0)
-#     labels = np.concatenate(labels, axis=0)
-#
-#     weibull_model.fit(activations, labels)
-#
-# #4
-# # 이미지 예측 시 OpenMax 사용
-# def predict_with_openmax(image, model, weibull_model, class_labels, alpha=10):
-#     features = extract_features(image, model)
-#
-#     openmax_probs = compute_openmax(features[0], weibull_model, alpha=alpha)
-#     predicted_class = np.argmax(openmax_probs)
-#
-#     if predicted_class == len(class_labels):  # Unknown class index
-#         return "Unknown", openmax_probs[predicted_class]
-#     else:
-#         return class_labels[predicted_class], openmax_probs[predicted_class]
-#OpenMax
-
 
 # 경로 설정
-model_weight_save_path = "pytorchToDjangoTest/resnet50_epoch_48_team1_loss_2153_acc_69_52.pth"
-num_classes = 5
+model_weight_save_path = "pytorchToDjangoTest/resnet50_epoch_10_240821_acc91.pth"
+num_classes = 2
 
 # ResNet-50 모델 정의 및 로드
 model = models.resnet50(pretrained=False)
@@ -192,8 +107,9 @@ class ImageClassificationView(APIView):
 
 
                 # 1조
-                class_labels = {0: '고양이', 1: '공룡', 2: '강아지',3: '꼬북이',4: '티벳여우'}
+                # class_labels = {0: '고양이', 1: '공룡', 2: '강아지',3: '꼬북이',4: '티벳여우'}
                 #3조
+                class_labels = {0: '망치', 1: '공업용가위'}
                 # class_labels = {0: '공구톱', 1: '공업용가위', 2: '그라인더', 3: '니퍼', 4: '드라이버'
                 #                 , 5: '망치', 6: '스패너', 7: '전동드릴', 8: '줄자', 9: '버니어 캘리퍼스'}
                 #2조
